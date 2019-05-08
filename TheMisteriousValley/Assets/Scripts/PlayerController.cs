9d31bbab99a0decc3bce7a22e8ee15fcdec8067b
zeekkeen <zekken2307@gmail.com>
2019-05-08 16:36:57 -0400

    prefrab

---
+++ TheMisteriousValley/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float vida = 100f;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void adelante()
+    {
+
+    }
+}
